83a30966d5ec497910b1308ff9a1206173ebaeb2
María Imbert <imbert.maria@hotmail.com>
2024-01-14 13:09:37 -0300

    create vue router + router links added + axios get + show data list

---
+++ src/views/PokemonsView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const pokemons = ref([]);
+
+const getPokemones = async () => {
+    try {
+        const data = await axios.get("https://pokeapi.co/api/v2/pokemon");
+        pokemons.value = data.data.results
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+getPokemones();
+</script>
+
+<template>
+    <h1>Pokemones</h1>
+    <ul >
+        <li v-for="poke in pokemons" >
+        {{ poke.name }}
+        </li>
+    </ul>
+</template>
